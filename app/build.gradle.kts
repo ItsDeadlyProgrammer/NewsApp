c129ed70e0d41f15bc445fadf0835fdfc14d8bba
harshuuuu <147650395+ItsDeadlyProgrammer@users.noreply.github.com>
2025-08-28 10:36:18 +0530

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,13 @@
+
+import java.util.Properties
+import java.io.FileInputStream
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -5,6 +15,8 @@ plugins {
     id("com.google.gms.google-services")
 
 }
+
+
 
 android {
     namespace = "com.example.news"
@@ -16,8 +28,9 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        val googleApiKey: String? = localProperties.getProperty("GOOGLE_API_KEY")
+        buildConfigField("String", "GOOGLE_API_KEY", "\"${googleApiKey ?: ""}\"")
     }
 
     buildTypes {
